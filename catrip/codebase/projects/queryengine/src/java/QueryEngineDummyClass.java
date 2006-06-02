0d17b5ff11d7e7140270230bacc80b8326de1778
srakkala <srakkala>
2006-06-02 16:41:02 +0000

    testing environment
    SVN-Revision: 101

--- catrip/codebase/projects/queryengine/src/java/QueryEngineDummyClass.java
+++ catrip/codebase/projects/queryengine/src/java/QueryEngineDummyClass.java
@@ -1,6 +1,7 @@
 /**
  * This is a dummy class for testing the project management framework.  
  * It should be removed for a "production" development environment.
+ *
  * @author Patrick McConnell
  */
 public class QueryEngineDummyClass
