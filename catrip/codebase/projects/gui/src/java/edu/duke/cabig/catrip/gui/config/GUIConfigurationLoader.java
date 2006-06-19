5f3dedf9ac35220be2759be449beb29f867f7f90
sanjeevagarwal <sanjeevagarwal>
2006-06-19 16:20:34 +0000

    GUI Configuration Files related classes. config File : caTRIP_Config.xml
    SVN-Revision: 148

---
+++ catrip/codebase/projects/gui/src/java/edu/duke/cabig/catrip/gui/config/GUIConfigurationLoader.java
@@ -0,0 +1,35 @@
+/*
+ * GUIConfigurationLoader.java
+ *
+ * Created on June 7, 2006, 4:41 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+ 
+package edu.duke.cabig.catrip.gui.config;
+
+import edu.duke.cabig.catrip.gui.security.LoginConfiguration;
+
+/**
+ *
+ * @author Sanjeev Agarwal
+ */
+public  class GUIConfigurationLoader {
+    
+    /**
+     * Creates a new instance of GUIConfigurationLoader
+     */
+    public GUIConfigurationLoader() {
+    }
+    
+   public GUIConfiguration getGUIConfiguration(){
+       return null;
+   }  
+    
+   public LoginConfiguration getLoginConfiguration(){
+       return null;
+   } 
+   
+   
+}
