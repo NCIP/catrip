df600d62af6de573efbd1bf715fad4eb49bcac7d
sanjeevagarwal <sanjeevagarwal>
2006-06-19 16:24:42 +0000

    Class for Exporting Query Results.
    SVN-Revision: 151

---
+++ catrip/codebase/projects/gui/src/java/edu/duke/cabig/catrip/gui/query/QueryResultExporter.java
@@ -0,0 +1,22 @@
+/*
+ * QueryResultExporter.java
+ *
+ * Created on June 7, 2006, 5:23 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.duke.cabig.catrip.gui.query;
+
+/**
+ *
+ * @author Sanjeev Agarwal
+ */
+public class QueryResultExporter {
+    
+    /** Creates a new instance of QueryResultExporter */
+    public QueryResultExporter() {
+    }
+    
+}
