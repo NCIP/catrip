6b26bdbea6ae87cba066edc48ad189827e450069
sanjeevagarwal <sanjeevagarwal>
2006-08-10 17:18:50 +0000

    Adding junit tests.
    SVN-Revision: 513

--- catrip/codebase/projects/gui/test/src/java/edu/duke/cabig/catrip/gui/discovery/XMLDomainModelRetrievalStrategyTest.java
+++ catrip/codebase/projects/gui/test/src/java/edu/duke/cabig/catrip/gui/discovery/XMLDomainModelRetrievalStrategyTest.java
@@ -21,36 +21,36 @@ public class XMLDomainModelRetrievalStrategyTest extends TestCase {
     public XMLDomainModelRetrievalStrategyTest(String testName) {
         super(testName);
     }
-    
+
     protected void setUp() throws Exception {
     }
-    
+
     protected void tearDown() throws Exception {
     }
-    
+
     public static Test suite() {
         TestSuite suite = new TestSuite(XMLDomainModelRetrievalStrategyTest.class);
         
         return suite;
     }
-    
+
     /**
      * Test of retrievDomainModel method, of class edu.duke.cabig.catrip.gui.discovery.XMLDomainModelRetrievalStrategy.
      */
     public void testRetrievDomainModel() {
         System.out.println("Running the retrieve domain model from domain model Metadata extracts");
-        ArrayList<ServiceMetaDataBean> alist = null;
+        ArrayList<ServiceMetaDataBean> alist = null; 
         try{
             XMLFileServiceLocator instance = new XMLFileServiceLocator();
             alist = instance.discoverAllLocalServices();
             
-            for (int i = 0; i < alist.size(); i++) {
+            for (int i = 0; i < alist.size(); i++) { 
                 ServiceMetaDataBean sBean = (ServiceMetaDataBean)alist.get(i);
-                XMLDomainModelRetrievalStrategy dr = new XMLDomainModelRetrievalStrategy((String)sBean.getDomainModelEndPointRef());
-                DomainModel model = dr.retrievDomainModel();
+                XMLDomainModelRetrievalStrategy xmlStrategy = new XMLDomainModelRetrievalStrategy((String)sBean.getDomainModelEndPointRef());  
+                DomainModel model = xmlStrategy.retrievDomainModel();  
                 assertTrue( model.getProjectShortName() != null );
             }
-            
+
         } catch(Exception ee){
             ee.printStackTrace();
             fail("The retrieve domain model test is failed.");
