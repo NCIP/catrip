e5019260d5d6d987d725cfb4cafbc13bdbfdf4e1
mccon012 <mccon012>
2006-08-10 12:40:21 +0000

    *** empty log message ***
    SVN-Revision: 502

---
+++ catrip/codebase/projects/caTissueCAEDataService/src/gov/nih/nci/cagrid/cae/service/globus/resource/ResourceConstants.java
@@ -0,0 +1,15 @@
+package gov.nih.nci.cagrid.cae.service.globus.resource;
+
+import javax.xml.namespace.QName;
+
+
+public interface ResourceConstants {
+	public static final String SERVICE_NS = "http://cagrid.nci.nih.gov/CAE/CAE";
+	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "CAEKey");
+	public static final QName RESOURCE_PROPERY_SET = new QName(SERVICE_NS, "CAEResourceProperties");
+
+	//Service level metadata (exposed as resouce properties)
+	public static final QName SERVICEMETADATA_MD_RP = new QName("gme://caGrid.caBIG/1.0/gov.nih.nci.cagrid.metadata", "ServiceMetadata");
+	public static final QName DOMAINMODEL_MD_RP = new QName("gme://caGrid.caBIG/1.0/gov.nih.nci.cagrid.metadata.dataservice", "DomainModel");
+	
+}
