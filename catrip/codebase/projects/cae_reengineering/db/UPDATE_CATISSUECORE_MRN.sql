4840fcd9b6db4f4fec5693b2c9a1a1cdfea2a9e4
srakkala <srakkala>
2006-09-15 18:16:44 +0000

    *** empty log message ***
    SVN-Revision: 691

--- catrip/codebase/projects/cae_reengineering/db/UPDATE_CATISSUECORE_MRN.sql
+++ catrip/codebase/projects/cae_reengineering/db/UPDATE_CATISSUECORE_MRN.sql
@@ -1,6 +1,8 @@
 update catissue_part_medical_id set site_id = 4 where identifier<41;
 update catissue_part_medical_id set site_id = 5 where identifier>40;
 
+update catissue_clinical_report set PARTICIPENT_MEDI_IDENTIFIER_ID=IDENTIFIER;
+
 update catissue_part_medical_id set medical_record_number = concat("DU",identifier,"00") where identifier<41;
 
 update catissue_part_medical_id set medical_record_number = concat("TT",identifier,"00") where identifier>40;
