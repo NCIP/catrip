586525f81498d9190287079af2348fc1f1105043
srakkala <srakkala>
2006-09-26 15:15:06 +0000

    *** empty log message ***
    SVN-Revision: 726

---
+++ catrip/codebase/projects/caTissueCAEDataService_NewIntroduce/src/gov/nih/nci/cagrid/cae/service/CAEImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.cagrid.cae.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ *  TODO:DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.0
+ * 
+ */
+public class CAEImpl extends CAEImplBase {
+
+	
+	public CAEImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
