66b205eb75b7f8cb6ba0ef444ac8c4bdb0f6de68
srakkala <srakkala>
2006-10-05 20:52:22 +0000

    *** empty log message ***
    SVN-Revision: 800

---
+++ catrip/codebase/projects/CaTissueCoreDataServiceV2/src/gov/nih/nci/cagrid/catissuecore/service/CaTissueCoreImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.cagrid.catissuecore.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ *  TODO:DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.0
+ * 
+ */
+public class CaTissueCoreImpl extends CaTissueCoreImplBase {
+
+	
+	public CaTissueCoreImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
