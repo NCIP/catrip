45b3311e639445ba2dadb9f6a90f80f7a312397a
srakkala <srakkala>
2006-10-05 21:05:27 +0000

    *** empty log message ***
    SVN-Revision: 802

---
+++ catrip/codebase/projects/CGEMSDataServiceV2/src/gov/nih/nci/cagrid/cgems/service/CGEMSImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.cagrid.cgems.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ *  TODO:DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.0
+ * 
+ */
+public class CGEMSImpl extends CGEMSImplBase {
+
+	
+	public CGEMSImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
