2fe7f5c2611ac022704abaab7d86e86cf0e88b5b
srakkala <srakkala>
2006-10-11 15:02:38 +0000

    *** empty log message ***
    SVN-Revision: 859

--- catrip/codebase/projects/CGEMSDataServiceV2/test/src/gov/nih/nci/catrip/service/test/TestCGEMS.java
+++ catrip/codebase/projects/CGEMSDataServiceV2/test/src/gov/nih/nci/catrip/service/test/TestCGEMS.java
@@ -4,22 +4,33 @@ import gov.nih.nci.cagrid.cgems.client.CGEMSClient;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
 import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
 import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
-
 import java.io.File;
 import java.io.FileInputStream;
 
+import java.io.IOException;
+
+import java.util.Properties;
+
 import junit.framework.TestCase;
-
 import org.globus.wsrf.encoding.ObjectDeserializer;
-
 import org.xml.sax.InputSource;
-
 
 public class TestCGEMS extends TestCase {
 
-    private String url = "http://localhost:8181/wsrf/services/cagrid/CGEMS";
-    private String clientConfig = "src/gov/nih/nci/cagrid/cgems/client/client-config.wsdd";
-    public static String CQL_FILES_DIR = "cql";//C:\\CVS-CodeBase\\catrip\\codebase\\projects\\CGEMSDataServiceV2\\test\\cql";
+    private static String url = "";
+    private static String clientConfig = "";
+    public static String CQL_FILES_DIR = "./cql";
+
+    static {
+        Properties properties = new Properties();
+        try {
+                properties.load(new FileInputStream(CQL_FILES_DIR+File.separator+"cql.properties"));
+            } catch (IOException e) {
+                e.printStackTrace();
+        }
+        url=properties.getProperty("serviceURL");
+        clientConfig=properties.getProperty("clientConfig");
+    }
 
     public TestCGEMS (String sTestName) {
         super(sTestName);
@@ -35,20 +46,33 @@ public class TestCGEMS extends TestCase {
 
     public CQLQuery getCQLQuery(String fileName)  throws Exception {
 
-        java.lang.Object obj = ObjectDeserializer.deserialize(new InputSource(new FileInputStream(new File(CQL_FILES_DIR+File.separator+fileName))),CQLQuery.class);
+        java.lang.Object obj = ObjectDeserializer.deserialize(new InputSource(new FileInputStream(CQL_FILES_DIR+File.separator+fileName)),CQLQuery.class);
         return (CQLQuery)obj;
 
     }
-    public void testGetParticipants()  throws Exception {
-         String fileName = "many-to-many.xml";
+
+    private void getParticipants(String fileName)  throws Exception {
+         //String fileName = "many-to-many.xml";
          CGEMSClient client = new CGEMSClient(url);
          CQLQueryResults results = client.query(getCQLQuery(fileName));
 
          CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results, new FileInputStream(new File(clientConfig)));
-
+		 System.out.println("Results for "+fileName);
          while (iter.hasNext()) {
             gov.nih.nci.caintegrator.domain.study.bean.StudyParticipant de = (gov.nih.nci.caintegrator.domain.study.bean.StudyParticipant) iter.next();
             System.out.println(de.getStudySubjectIdentifier() + "     " + de.getInstitutionName());
          }
+         System.out.println("---------------------");
+    }
+    public void testGetParticipants() throws Exception {
+        getParticipants("Participants.xml");
+    }
+
+    public void testGetParticipantWithFilterOnSpecimen() throws Exception {
+        getParticipants("ParticipantWithFilterOnSpecimen.xml");
+    }
+
+    public void testGetParticipantWithFilterOnSNPAnalysisGroup() throws Exception {
+        getParticipants("ParticipantWithFilterOnSNPAnalysisGroup.xml");
     }
 }
