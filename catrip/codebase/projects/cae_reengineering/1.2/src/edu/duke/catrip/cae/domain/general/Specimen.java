ed0f1602d33ef9de66c739cdbf72b42a0f64e166
srakkala <srakkala>
2006-10-12 12:22:04 +0000

    added identifier attribute
    SVN-Revision: 888

--- catrip/codebase/projects/cae_reengineering/1.2/src/edu/duke/catrip/cae/domain/general/Specimen.java
+++ catrip/codebase/projects/cae_reengineering/1.2/src/edu/duke/catrip/cae/domain/general/Specimen.java
@@ -9,30 +9,38 @@ import java.util.Set;
  * <!-- LICENSE_TEXT_START -->
  * <!-- LICENSE_TEXT_END -->
  */
- 
+
   /**
-   * Harmonized Specimen class . This is part of CAE backbone object hrmonization . Attributes of SpecimenIdentifiers 
-   * class of CAE are harmonized into this Specimen class . 
-   * 
+   * Harmonized Specimen class . This is part of CAE backbone object hrmonization . Attributes of SpecimenIdentifiers
+   * class of CAE are harmonized into this Specimen class .
+   *
    */
 
-public  class Specimen 
+public  class Specimen
    extends edu.pitt.cabig.cae.domain.general.AnnotatableEntity{
 	private static final long serialVersionUID = 1234567890L;
 	private Set specimenCollection;
-	   
+
 	public Specimen(){
 
 	}
-	   
+
 	   private java.lang.String surgicalLabel;
+	   private String identifier;
+
 	   public  java.lang.String getSurgicalLabel(){
 	      return surgicalLabel;
 	   }
 	   public void setSurgicalLabel( java.lang.String surgicalLabel){
 	      this.surgicalLabel = surgicalLabel;
 	   }
-	
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
 
 	public Set getSpecimenCollection() {
 		return specimenCollection;
@@ -42,31 +50,31 @@ public  class Specimen
 		this.specimenCollection = specimenCollection;
 	}
 
-	
+
 
 		public boolean equals(Object obj){
 			boolean eq = false;
 			if(obj instanceof Specimen) {
-				Specimen c =(Specimen)obj; 			 
-				Long thisId = getId();		
-				
+				Specimen c =(Specimen)obj;
+				Long thisId = getId();
+
 					if(thisId != null && thisId.equals(c.getId())) {
 					   eq = true;
-				    }		
-				
+				    }
+
 			}
 			return eq;
 		}
-		
+
 		public int hashCode(){
 			int h = 0;
-			
+
 			if(getId() != null) {
 				h += getId().hashCode();
 			}
-			
+
 			return h;
 	}
-	
-	
+
+
 }
