56e917eab1010c95bdc6a41a3f026dbfeea4dd66
srakkala <srakkala>
2006-10-12 12:18:25 +0000

    mvr was misspelled . changed to MVR
    SVN-Revision: 883

--- catrip/codebase/projects/cae_reengineering/1.2/src/edu/pitt/cabig/cae/domain/general/AdditionalFindings.java
+++ catrip/codebase/projects/cae_reengineering/1.2/src/edu/pitt/cabig/cae/domain/general/AdditionalFindings.java
@@ -6,60 +6,60 @@ package edu.pitt.cabig.cae.domain.general;
  * <!-- LICENSE_TEXT_START -->
  * <!-- LICENSE_TEXT_END -->
  */
- 
+
   /**
    * Significant pathologic findings that are present in addition to the tumor.
    */
 
-public  class AdditionalFindings 
+public  class AdditionalFindings
     extends AnnotationSet
-	implements java.io.Serializable 
+	implements java.io.Serializable
 {
 	private static final long serialVersionUID = 1234567890L;
 
-	
-	   
+
+
 	   private java.lang.String otherFindings;
-	   private java.lang.String mvr;
+	   private java.lang.String MVR;
 	   public  java.lang.String getOtherFindings(){
 	      return otherFindings;
 	   }
 	   public void setOtherFindings( java.lang.String otherFindings){
 	      this.otherFindings = otherFindings;
 	   }
-	
 
-	
+
+
 
 		public boolean equals(Object obj){
 			boolean eq = false;
 			if(obj instanceof AdditionalFindings) {
-				AdditionalFindings c =(AdditionalFindings)obj; 			 
-				Long thisId = getId();		
-				
+				AdditionalFindings c =(AdditionalFindings)obj;
+				Long thisId = getId();
+
 					if(thisId != null && thisId.equals(c.getId())) {
 					   eq = true;
-				    }		
-				
+				    }
+
 			}
 			return eq;
 		}
-		
+
 		public int hashCode(){
 			int h = 0;
-			
+
 			if(getId() != null) {
 				h += getId().hashCode();
 			}
-			
+
 			return h;
 	}
-		public java.lang.String getMvr() {
-			return mvr;
+		public java.lang.String getMVR() {
+			return MVR;
 		}
-		public void setMvr(java.lang.String mvr) {
-			this.mvr = mvr;
+		public void setMVR(java.lang.String MVR) {
+			this.MVR = MVR;
 		}
-	
-	
+
+
 }
