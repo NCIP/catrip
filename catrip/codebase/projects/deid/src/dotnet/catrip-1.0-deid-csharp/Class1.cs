787414ca1651e2c4c3e09d560beabec6d4343bc1
mccon012 <mccon012>
2006-10-30 15:23:32 +0000

    initial commit
    SVN-Revision: 1078

---
+++ catrip/codebase/projects/deid/src/dotnet/catrip-1.0-deid-csharp/Class1.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+using catrip_1._0_deid_csharp.WebReference;
+
+namespace catrip_1._0_deid_csharp
+{
+	internal class AcceptAllCertificatePolicy : ICertificatePolicy
+	{
+		public AcceptAllCertificatePolicy()
+		{
+		}
+
+		public bool CheckValidationResult(ServicePoint sPoint,
+			X509Certificate cert, WebRequest wRequest,int certProb)
+		{
+			// Always accept
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+	class Class1
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			ServicePointManager.CertificatePolicy = new AcceptAllCertificatePolicy();
+
+			DeIdService service = new DeIdService();
+			
+			security security = new security();
+			security.user = "someuser";
+			security.password = "somepassword";
+			service.securityValue = security;
+
+			deid input = new deid();
+			input.phi = "hi there";
+
+			deidResponse output = new deidResponse();
+
+			try 
+			{
+				output = service.deid(null, input); 
+				Console.WriteLine(output.@return);
+			} 
+			catch (Exception e) 
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+				Console.WriteLine(e.InnerException);
+			}
+		}
+	}
+}
