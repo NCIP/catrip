b9d56164f58b63fceaace054cca4546c67c6dc05
bmason <bmason>
2007-01-26 14:43:36 +0000

    *** empty log message ***
    SVN-Revision: 1401

--- catrip/codebase/projects/tumor registry/db/delete_records.sql
+++ catrip/codebase/projects/tumor registry/db/delete_records.sql
@@ -4,11 +4,12 @@ delete from collaborative_staging;
 delete from distant_site;
 delete from disease_extent;
 delete from first_course_trmt_summary;
-delete from follow_up;
 delete from patient_identifier;
 
 delete from recurrence;
+delete from follow_up;
 delete from diagnosis;
+delete from follow_up;
 delete from patient;
 delete from address;
 
