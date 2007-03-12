88ec3f26f8d0b76d3f6a706c19d4eda4ae251ed7
srakkala <srakkala>
2007-03-12 16:15:48 +0000

    no message
    SVN-Revision: 1555

--- catrip/codebase/projects/gui/src/java/edu/duke/cabig/catrip/gui/security/AuthenticationManager.java
+++ catrip/codebase/projects/gui/src/java/edu/duke/cabig/catrip/gui/security/AuthenticationManager.java
@@ -1,6 +1,12 @@
 
 package edu.duke.cabig.catrip.gui.security;
 
+import gov.nih.nci.cagrid.authentication.bean.BasicAuthenticationCredential;
+import gov.nih.nci.cagrid.authentication.bean.Credential;
+import gov.nih.nci.cagrid.authentication.client.AuthenticationServiceClient;
+import java.rmi.RemoteException;
+import org.apache.axis.types.URI;
+
 /**
  * Authentication Manager to authenticate the user form the login screen.
  *
@@ -13,13 +19,34 @@ public class AuthenticationManager {
     }
     
     /** Go to the selected IDP and authenticate the user. Get the proxy cert or SAML and store. */
-    public static boolean authenticate(String loginId, String password, String idProviderUrl){
-        // go to the actual service and check the credentials..
-        // also immediately store the proxy certificate obtained from the IDP
-        // - someone.authenticate returns a X.509 Certificate
-        // - CertificateStore.storeCertificate: boolean "May return false if the proxy certificate is duplicate.."
-
-        return true;
+    public static boolean authenticate(String loginId, String password, String idProviderUrl) {
+        
+        idProviderUrl="https://localhost:8443/wsrf/services/cagrid/AuthenticationService";
+        
+        boolean login = false;
+        /*
+        try {
+            // go to the actual service and check the credentials..
+            // also immediately store the proxy certificate obtained from the IDP
+            // - someone.authenticate returns a X.509 Certificate
+            // - CertificateStore.storeCertificate: boolean "May return false if the proxy certificate is duplicate.."
+            AuthenticationServiceClient client = new AuthenticationServiceClient(idProviderUrl);
+            BasicAuthenticationCredential bac = new BasicAuthenticationCredential();
+            bac.setUserId(loginId);
+            bac.setPassword(password);
+            Credential cred = new Credential();
+            cred.setBasicAuthenticationCredential(bac);
+            String xml = client.authenticate(cred).getXml();
+            System.out.println(xml);
+            login=true;
+        } catch (URI.MalformedURIException ex) {
+            ex.printStackTrace();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+         **/
+        login=true;
+        return login;
     }
     
     
